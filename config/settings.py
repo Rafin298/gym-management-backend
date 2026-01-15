ec06c1ac8b0d4bb376aae34047d2a49e4327c273
Rafin298 <rafinkhan298@gmail.com>
2026-01-15 10:33:13 +0600

    add rate limiting

--- config/settings.py
+++ config/settings.py
@@ -156,6 +156,14 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    "DEFAULT_THROTTLE_CLASSES": (
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ),
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "10/min",     # not logged in
+        "user": "120/min",    # authenticated users
+    },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
     'EXCEPTION_HANDLER': 'rest_framework.views.exception_handler',
